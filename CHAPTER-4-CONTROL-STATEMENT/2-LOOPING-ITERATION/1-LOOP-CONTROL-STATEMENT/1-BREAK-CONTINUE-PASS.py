051b515f2604f994a3a5f6846aeda72a1a956d7e
ARYAN <whyonlyaryan1554@gmail.com>
2026-08-31 19:25:09 +0530

    small changes

---
+++ CHAPTER-4-CONTROL-STATEMENT/2-LOOPING-ITERATION/1-LOOP-CONTROL-STATEMENT/1-BREAK-CONTINUE-PASS.py
@@ -0,0 +1,6 @@
+for i in range(1,6):
+    if i ==2:
+        break
+    print(i)
+else :
+    print("program is over bro")
---
+++ PRACTICE-PROBLEM/6-PALINDROME.py
@@ -0,0 +1,21 @@
+a = str(input("enter the string bro "))
+n = len(a)
+print (n)
+b= n-1
+while(b !=0):
+    print(b)
+    b = b-1
+
+    
+
+# b = n-1
+
+# print(*range(5))
+
+# print(a)
+
+# print()
+# if a == b:
+#     print(" found the palindromen bro")
+# else:
+#     print(" better luck next time bro")    
